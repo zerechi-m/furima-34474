e49853b195ad74e2c05132104565ec7d7a2c09d2
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-24 15:52:00 +0900

    userモデルの正規表現追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "漢字・ひらがな・カタカナ可能"} do
+    validates :first_name
+    validates :last_name
+  end
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"全角文字を使用してください"} do
+  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "全角文字を使用してください"} do
     validates :first_name_kana  
     validates :last_name_kana
   end
