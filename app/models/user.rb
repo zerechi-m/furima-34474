04bf7807198de06268e7b15c2ab7813cad1f0f67
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-24 15:24:02 +0900

    userモデルのvalidates追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"全角文字を使用してください"} do
+    validates :first_name_kana  
+    validates :last_name_kana
+  end
   validates :nickname, presence: true
   validates :birthday, presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "passwordには半角英数字混合で入力してください"}
 end
