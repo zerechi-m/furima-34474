2e82e4ecc5d179856c98a189ab66e885808593d6
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-24 17:49:36 +0900

    ログイン機能の実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name   {Faker::}
+    last_name
+    first_name_kana  
+    last_name_kana
+    nickname
+    birthday
+    password
   end
+    
+ 
 end
