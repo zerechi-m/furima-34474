ae10f18a147c37bcc766bcd693e2adc4d2af2b84
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-25 16:25:20 +0900

    itemモデルのバリデーション記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :describe
+    validates :status_id
+    validates :category_id
+    validates :delivery_fee_id
+    validates :delivery_day_id
+    validates :prefecture_id
+    validates :user
+  end
+
+  has_one_attached :image
 end
