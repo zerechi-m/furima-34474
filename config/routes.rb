7e163502f04a6131ac468db7f2f93ff90a28e363
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-26 18:04:52 +0900

    items#edit,updateのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :show, :update]
+  resources :items, only: [:new, :create, :show, :edit, :update]
 end
