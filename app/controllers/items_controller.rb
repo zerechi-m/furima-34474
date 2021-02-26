1acd74a18928af48bae7fafbadf3bc84826e7538
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-26 20:17:10 +0900

    items#destroyの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]  # インスタンスの生成
-  before_action :user_match, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]  # インスタンスの生成
+  before_action :user_match, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order(id: "DESC")
@@ -34,6 +34,11 @@ class ItemsController < ApplicationController
     else
       render action: :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
