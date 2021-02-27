e078b1f535a35b208a288b55074bceb5abbaf038
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-27 16:37:56 +0900

    order#createアクションの設定

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -2,4 +2,18 @@ class OrderController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).merge(user_id: current_user[:id], item_id: params[:item_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :order, only: [:index]
+    resources :order, only: [:index, :create]
   end
 end
