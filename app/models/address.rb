9839c7cb6d63bd3c0170a5c5fe8e9eafd7e3c385
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-27 21:12:06 +0900

    formオブジェクトの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,4 @@
 class Address < ApplicationRecord
-  with_options presence: true do
-    validates :postal_code
-    validates :prefecture_id
-    validates :municipality
-    validates :address
-    validates :phone_number
-    validates :order_id
-  end
 
   belongs_to :order
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,4 @@
 class Order < ApplicationRecord
-  with_options presence: true do
-   validates :user_id
-   validates :item_id
-  end
 
   belongs_to :user
   belongs_to :item
