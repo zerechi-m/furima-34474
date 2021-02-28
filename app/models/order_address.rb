4d070b42780df9370395243390f28edec9dbba7a
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-28 13:03:37 +0900

    rubocop によるコード整形

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,21 +1,22 @@
-class OrderAddress 
+class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :order_id, :item_id, :user_id, :token
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :order_id, :item_id, :user_id,
+                :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}+\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}+\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :municipality
     validates :address
-    validates :phone_number, format: {with: /\A\d{10,11}+\z/}
+    validates :phone_number, format: { with: /\A\d{10,11}+\z/ }
     validates :token
-   end
+  end
 
-   def save
-      order = Order.create(item_id: item_id, user_id: user_id)
-      Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality , address: address, building: building, phone_number: phone_number, order_id: order.id)
-   end
-
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address,
+                   building: building, phone_number: phone_number, order_id: order.id)
+  end
 end
