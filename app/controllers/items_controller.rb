e8837f565b6f364e55cb09d25c297956a7e5e455
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-28 16:56:35 +0900

    商品soldout時のアクション分岐設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]  # インスタンスの生成
-  before_action :user_match, only: [:edit, :update, :destroy]
+  before_action :user_match, only: [:edit, :update, :destroy]       # 現在のユーザと出品者の合致
+  before_action :soldout, only: [:edit, :update, :destroy]          # 商品がsoldoutかの分岐
 
   def index
     @items = Item.all.order(id: 'DESC')
@@ -24,9 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.order.present?
-      redirect_to root_path
-    end
+
   end
 
   def update
@@ -50,6 +49,12 @@ class ItemsController < ApplicationController
 
   def user_match
     redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def soldout
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def item_params
